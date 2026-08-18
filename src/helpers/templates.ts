0e3c9f9413a6e64cd41c2116531abc5731d7239f
dcapslock <dcapes-davis@outlook.com.au>
2026-08-19 09:36:53 +1000

    build(deps): Bump home-assistant-javascript-templates to 8.0.0 with required changes and tests.

--- src/helpers/templates.ts
+++ src/helpers/templates.ts
@@ -10,7 +10,7 @@ export function getJSTemplateRenderer(variables: Record<string, unknown> = {}, r
             refs,
             refsVariableName: 'variables'
         }
-    ).getRenderer();
+    ).getRenderer().then((renderer) => renderer.init().then(() => renderer));
 }
 
 export function isJSTemplate(template: unknown): boolean {
