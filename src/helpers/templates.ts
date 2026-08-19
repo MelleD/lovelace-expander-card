e6b0dc360f2dee392e0c637923082cb662ee921c
dcapslock <dcapes-davis@outlook.com.au>
2026-08-20 07:18:50 +1000

    chore(ci): Update home-assistant-javascript-templates to 8.0.0-beta.1

--- src/helpers/templates.ts
+++ src/helpers/templates.ts
@@ -10,7 +10,7 @@ export function getJSTemplateRenderer(variables: Record<string, unknown> = {}, r
             refs,
             refsVariableName: 'variables'
         }
-    ).getRenderer().then((renderer) => renderer.init().then(() => renderer));
+    ).getRenderer().then((renderer) => renderer.init());
 }
 
 export function isJSTemplate(template: unknown): boolean {
